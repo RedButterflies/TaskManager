eedac5a130658bc89412ca65308ecedaa180b895
Aleksandra Olejarz <s99208@pollub.edu.pl>
2024-11-28 14:54:22 +0100

    Added crucial comments to GlobalExceptionHandler for better understanding of the code

--- src/main/java/com/example/taskmanager/GlobalExceptionHandler.java
+++ src/main/java/com/example/taskmanager/GlobalExceptionHandler.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    //Obsluguje wyjatki walidacyjne(ConstraintViolationException)
     @ExceptionHandler(ConstraintViolationException.class)
     public String handleConstraintViolationException(ConstraintViolationException ex, Model model) {
+        //Dodaje komunikat o bledzie walidacji do modelu, aby mozna bylo wyświetlic go w widoku
         model.addAttribute("errorMessage", "Validation error: " + ex.getMessage());
-        return "error-page";
+        return "error-page"; // Przekierowuje uzytkownika na strone bledu
     }
 }
